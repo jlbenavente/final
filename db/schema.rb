73963b825e9d1cc68f82e738597dbd61a2698d01
jlbenavente <jlbenaventecanales@gmail.com>
2019-02-05 20:18:11 -0300

    model picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_231106) do
+ActiveRecord::Schema.define(version: 2019_02_05_231627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 2019_02_05_231106) do
     t.string "specialty"
     t.datetime "end_date"
     t.integer "budget"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "name"
+    t.string "place"
+    t.string "photo"
+    t.datetime "start_date"
+    t.datetime "end_time"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
